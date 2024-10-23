a8ba7831763de85a9ece85dbb07908fff51ab0d7
haydencj <haydenjohns02@gmail.com>
2024-10-23 17:27:42 -0500

    setup basic authentication

---
+++ app/components/auth/RequireAuth.tsx
@@ -0,0 +1,16 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Navigate } from "@remix-run/react";
+
+export function RequireAuth({ children }: { children: React.ReactNode }) {
+  const { isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
--- app/root.tsx
+++ app/root.tsx
@@ -1,45 +1,48 @@
+import { Auth0Provider } from "@auth0/auth0-react";
+import type { LoaderFunction } from "@remix-run/node";
 import {
   Links,
+  LiveReload,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
 } from "@remix-run/react";
-import type { LinksFunction } from "@remix-run/node";
 
-import "./tailwind.css";
+export const loader: LoaderFunction = async () => {
+  return {
+    env: {
+      AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,
+      AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+      AUTH0_CALLBACK_URL: process.env.AUTH0_CALLBACK_URL,
+    },
+  };
+};
 
-export const links: LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
+export default function App() {
+  const data = useLoaderData<typeof loader>();
 
-export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <head>
-        <meta charSet="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
       </head>
       <body>
-        {children}
+        <Auth0Provider
+          domain={data.env.AUTH0_DOMAIN}
+          clientId={data.env.AUTH0_CLIENT_ID}
+          authorizationParams={{
+            redirect_uri: data.env.AUTH0_CALLBACK_URL,
+          }}
+        >
+          <Outlet />
+        </Auth0Provider>
         <ScrollRestoration />
         <Scripts />
+        <LiveReload />
       </body>
     </html>
   );
-}
-
-export default function App() {
-  return <Outlet />;
 }
