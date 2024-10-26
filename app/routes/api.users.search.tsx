ff65cbeab65df6b4f3a78a207b58d1ec4aa92d8e
Andrew McLaughlin <andrewmcl6081@gmail.com>
2024-10-25 22:25:58 -0500

    Added API route for fetching all users

---
+++ app/routes/api.users.search.tsx
@@ -0,0 +1,20 @@
+import { User } from "@prisma/client";
+import { json, type LoaderFunction } from "@remix-run/node";
+import { UserService } from "~/services/user.server";
+
+export type UsersSearchLoaderData = {
+  users: User[];
+};
+
+export const loader: LoaderFunction = async ({ request }) => {
+  const url = new URL(request.url);
+  const currentUserId = url.searchParams.get("currentUserId");
+
+  if (!currentUserId) {
+    throw new Error("Current user ID is required.");
+  }
+
+  // Get all users except current user
+  const users = await UserService.getAllUsers(currentUserId);
+  return json({ users });
+}
